91f4d06f7a8d0f0acc1695b20a36353156b211bf
Viktor <elfotito@gmail.com>
2026-08-05 10:11:03 -0400

    Fase 1 Agregar Descuentos

--- src/components/admin/DescuentosAdmin.jsx
+++ src/components/admin/DescuentosAdmin.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useMemo } from 'react';
-import axios from '../api/axios'; // 👈 AJUSTA esta ruta a donde esté tu instancia real
+import axios from '../../api/axios'; // 👈 AJUSTA esta ruta a donde esté tu instancia real
 import DescuentoForm from './DescuentosForm';
 import './DescuentosAdmin.css';
 
