f1e761f124896bd6f2f578267048f0807c8ef1e7
Viktor <elfotito@gmail.com>
2026-08-10 12:06:31 -0400

    Fase 1 Ajustando Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -450,7 +450,7 @@ function Navbar() {
                     fontWeight: "bold" 
                   }}>{cantidadItems}</span>
             </div>
-            <span className="cart-price" style={{ fontSize: "0.875rem" }}>$0.00</span>
+            <span className="cart-price" style={{ fontSize: "0.7rem", marginTop: "-4px" }}>$0.00</span>
           </Link>
         </div>
 
