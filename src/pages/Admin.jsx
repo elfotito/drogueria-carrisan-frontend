0e69d6f35cccfd4a1ac0f5110e99ec8e5137a629
Viktor <elfotito@gmail.com>
2026-08-11 11:15:38 -0400

    Fase 1 Ajustando Catalogo

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -5,7 +5,7 @@ import ProductosAdmin from '../components/admin/ProductosAdmin'
 import UsuariosAdmin from '../components/admin/UsuariosAdmin'
 import EstadoCuentaAdmin from '../components/admin/EstadoCuentaAdmin'
 import DescuentosPanel from '../components/admin/DescuentosAdmin'
-import '../styles/Admin.css'
+import './Admin.css'
 
 function Admin() {
   const [seccionActiva, setSeccionActiva] = useState('tasa')
