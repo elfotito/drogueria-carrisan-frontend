80dd384fd545262dfbd3cdb8683d03a3a5b9f8e5
Viktor <elfotito@gmail.com>
2026-08-11 11:42:38 -0400

    Fase 1 Ajustando Admin

--- src/components/admin/TasaCambio.jsx
+++ src/components/admin/TasaCambio.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import api from '../../api/axios'
-import '../styles/admin/TasaCambio.css'
+import './TasaCambio.css'
 
 function TasaCambio() {
   const [tasaActual, setTasaActual] = useState(null)
