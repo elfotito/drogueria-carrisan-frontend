851607a2d69ab424e2b88ebe4225b2eedddbf4f8
Viktor <elfotito@gmail.com>
2026-08-13 17:17:49 -0400

    Fase 1 Ajustando Diseño General

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   build: {
     chunkSizeWarningLimit: 1500, // 1.5 MB
-  },
-  resolve: {
-    tsconfigPaths: true,
   },
 })
