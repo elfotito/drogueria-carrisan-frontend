274c325933a22d0d96cb8baf6ea519b85182fcf3
elfotito <49953276+elfotito@users.noreply.github.com>
2026-08-16 02:08:21 -0400

    Actualizar main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from "@/components/ui/provider"
+import { Provider } from "./components/ui/provider"
 import './index.css'
 import App from './App.jsx'
+import { Toaster } from './components/ui/toaster'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <Provider>
+     <Toaster />
       <App />
     </Provider>
     </BrowserRouter>
