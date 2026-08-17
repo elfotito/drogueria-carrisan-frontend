e453ddf78a17f29bf5a3255118fa94922adf23e6
Viktor <elfotito@gmail.com>
2026-08-17 15:51:00 -0400

    Fase 1 Agregando Comprobante Drive

--- src/components/paginas-principales/Layoutpaginaprincipal.jsx
+++ src/components/paginas-principales/Layoutpaginaprincipal.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 import { Menu, X, ChevronRight, LogOut } from 'lucide-react'
 import { useAuth } from '../../context/AuthContext'
 import BottomNav from '../BottomNav'
-import { NAV_PAGINAS_PRINCIPALES } from './navPaginasPrincipales'
+import { NAV_PAGINAS_PRINCIPALES } from './Navpaginasprincipales'
 import './Layoutpaginaprincipal.css'
 
 
