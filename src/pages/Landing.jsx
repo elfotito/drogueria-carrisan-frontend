e1cfa5c8ed23242024d7a2dc26172467d0c9e0e9
Viktor <elfotito@gmail.com>
2026-08-18 13:30:52 -0400

    Fase 1 Agregando Comprobante Drive

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -205,7 +205,7 @@ function Landing() {
       <section className="landing-hero">
         <div className="landing-hero__contenido">
           <img 
-      src="https://fqeshthtycmzgyibiurq.supabase.co/storage/v1/object/sign/dcimages/hero.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9mZGI0NTZkYy04YTAyLTQ3Y2EtYjc4Yy0yMWZmMWExOWM0MmEiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJkY2ltYWdlcy9oZXJvLnBuZyIsInNjb3BlIjoiZG93bmxvYWQiLCJpYXQiOjE3ODcwNjk2NjksImV4cCI6MTk0NDc0OTY2OX0.GQ74kvd4FQPZFaEDPcBRkiBZTU-TYqoktWjJimFwNCI" 
+      src="https://fqeshthtycmzgyibiurq.supabase.co/storage/v1/object/sign/dcimages/hero.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9mZGI0NTZkYy04YTAyLTQ3Y2EtYjc4Yy0yMWZmMWExOWM0MmEiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJkY2ltYWdlcy9oZXJvLnBuZyIsInNjb3BlIjoiZG93bmxvYWQiLCJpYXQiOjE3ODcwNzM4NDcsImV4cCI6MTk0NDc1Mzg0N30.wIXwMjprwF431A1_lgmNp_xBiSljeJkYwAVoRh3S_Ig" 
       alt="Hero image - Respaldamos al Medico Venezolano"
       className="landing-hero__imagen"
     />
