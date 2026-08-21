1207e2576f7bfb6150d685eb1a021bd2c50f7b80
Viktor <elfotito@gmail.com>
2026-08-21 17:16:10 -0400

    Fase 1 Reestructurando Modulo Administrativo

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from "./components/ui/provider"
 import './index.css'
+import './fonts.css' 
 import App from './App.jsx'
 import { Toaster } from './components/ui/toaster'
 
