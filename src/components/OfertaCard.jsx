1ef3d1a3e23099fd0a77010f4c487d0eaabd6f4c
elfotito <49953276+elfotito@users.noreply.github.com>
2026-08-22 00:28:23 -0400

    Actualizar OfertaCard.jsx

--- src/components/OfertaCard.jsx
+++ src/components/OfertaCard.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Plus, Check } from 'lucide-react'
 import { useAuth } from '../context/AuthContext'
-import { useCarrito } from '../context/CarritoContext'
+import { useCarrito } from '../context/CartContext'
 import { getEstadoProducto } from '../utils/productoEstado'
 import './OfertaCard.css'
 
