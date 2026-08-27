c339b197292d0597bd0250b6835a4c0b84d3485b
elfotito <49953276+elfotito@users.noreply.github.com>
2026-08-27 07:25:55 -0400

    Actualizar Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import { useAuth } from '../context/AuthContext'
 import DashboardMobile from '../components/DashboardMobile'
+import CookieConsent from '../components/CookieConsent'
+
 
 function Home() {
   const { user } = useAuth()
 
   return <DashboardMobile user={user} />
+      {user && <CookieConsent />}
 }
 
 export default Home
